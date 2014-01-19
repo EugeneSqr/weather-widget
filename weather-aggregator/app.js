df976418a93b45f9a23951917630dcf119c030da
Femel <eskurihin@gmail.com>
2014-01-19 13:16:26 +0400

    image builder has been added

--- weather-aggregator/app.js
+++ weather-aggregator/app.js
@@ -1,6 +1,7 @@
 var parseString = require('xml2js').parseString;
 var request = require('request');
 var extractor = require('./extractor.js');
+var imageBuilder = require('./imageBuilder.js');
 
 var baseUrl = 'http://export.yandex.ru/weather-ng/forecasts/';
 var cities = Object.freeze( {
@@ -9,12 +10,14 @@ var cities = Object.freeze( {
         Peter: 26063
 });
 
+      
 request({ uri: buildWeatherUrl(cities.Nino) },
         function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                         parseString(body, function (err, result) {
                                 var extracted = extractor.extract(result);
                                 console.dir(extracted);
+                                imageBuilder.build(extracted);
                         });
                 }
         });
